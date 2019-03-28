24e996105f4e8507ea4da4072fe5d45c0fca9ea7
Morlay <morlay.null@gmail.com>
2019-03-28 16:11:12 +0800

    refactor

--- env_var.go
+++ env_var.go
@@ -1,54 +1,38 @@
 package envconf
 
-import (
-	"strings"
-)
-
 // mask value string for security value
 type SecurityStringer interface {
 	SecurityString() string
 }
 
-func EnvVarFromKeyValue(key string, value string) *EnvVar {
-	envVar := &EnvVar{
-		KeyPath:    key,
-		Value:      value,
-		ShouldConf: true,
-	}
-
-	keyParts := strings.Split(key, "__")
-
-	if len(keyParts) == 2 {
-		envVar.KeyPath = keyParts[1]
-		if strings.Index(envVar.KeyPath, "_") == 0 {
-			envVar.KeyPath = envVar.KeyPath[1:]
-			envVar.ShouldConf = false
-		}
-		if strings.Contains(keyParts[0], "U") {
-			envVar.IsUpstream = true
-		}
-	}
-
-	return envVar
-}
-
-type EnvVarMeta struct {
-}
-
 type EnvVar struct {
-	KeyPath    string
-	Value      string
-	Mask       string
-	ShouldConf bool
+	KeyPath string
+	Value   string
+	Mask    string
+
+	Optional   bool
 	IsUpstream bool
+	IsCopy     bool
+	IsExpose   bool
+}
+
+func (envVar *EnvVar) metaFromFlags(flags map[string]bool) {
+	envVar.Optional = false
+
+	if flags["opt"] {
+		envVar.Optional = true
+	}
+	if flags["upstream"] {
+		envVar.IsUpstream = true
+	}
+	if flags["copy"] {
+		envVar.IsCopy = true
+	}
+	if flags["expose"] {
+		envVar.IsExpose = true
+	}
 }
 
 func (envVar *EnvVar) Key(prefix string) string {
-	if envVar.IsUpstream {
-		prefix += "U"
-	}
-	if !envVar.ShouldConf {
-		prefix += "_"
-	}
 	return prefix + "__" + envVar.KeyPath
 }
